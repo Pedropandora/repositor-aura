12131cd0c4628423892cf4582376513ac51ec782
Pedropandora <pedro.henrique.lima21@escola.pr.gov.br>
2026-07-06 11:32:57 -0300

    68

---
+++ main.js
@@ -0,0 +1,15 @@
+const botoesm =document.querySelectorAl("botton")
+console.log(botoes)
+
+botoes.array.forEach(function (botoes){
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado)
+
+    function botaoClicado (){
+        console.log("botao Clicado") 
+        let texto = botao.querySelector("span")
+        if (curtiu == false){
+            texto.textContent++
+        }
+    }
+})
